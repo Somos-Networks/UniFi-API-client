8595f3759aed03eb7ebfa078a9572391942f6431
Forrest Heath 3 <forrest@pareto-design.com>
2020-05-13 15:07:26 -0500

    fix: remove controller url extra /

--- src/config.php
+++ src/config.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $controlleruser     = 'forrestheath3';
 $controllerpassword = 'redunifi';
-$controllerurl      = 'https://52.203.37.193/:8443';
+$controllerurl      = 'https://52.203.37.193:8443';
 $controllerversion  = '5.12.35';
 $debug = false;
 $site_id = 'default';
